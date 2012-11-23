0e4e3afb9d46c6e23561bd18944f1d122b7b75ff
Justen Doherty <phpchap@gmail.com>
2012-11-23 10:36:11 +0000

    revision release (v.0.0.2)

--- bump_github_revision.sh
+++ bump_github_revision.sh
@@ -3,12 +3,14 @@
 #############################
 # http://en.wikipedia.org/wiki/Software_versioning
 
+# check we have called the script with valid parameters
 if [ ! $# == 1 ]; then
   echo "Usage: '$0 (major|minor|revision)' - increment the github tag according to the release type"
   exit
 fi
 
-echo "fetching the tags from github..."
+# fetch the tags from repo in case anyone else is bumping number
+echo "fetching the tags from repo..."
 git fetch --tags
 
 # what kind of a release is this?
@@ -22,10 +24,17 @@ REVISION=`git describe | cut -d "-" -f1 | cut -d "." -f4`
 # whats the current version were on
 CURRENTVERSION="v."$MAJOR"."$MINOR"."$REVISION
 
-echo "-------"
-echo "Connecting Travel Version Script"
+# get the current directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# create the release directory if not exists
+mkdir -p $DIR/releases/
+
+# display version and bump release (and credit to me!)
+echo "------------------------------------------------------------"
+echo "Git Versioning Script by: Justen Doherty (phpchap@gmail.com)"
 echo "Current Version: "$CURRENTVERSION
-echo "-------"
+echo "-------------------------------------------------------------"
 echo "we have a '"$BUMP"' release..."
 
 # depending on the version type, increment the counter
@@ -47,20 +56,26 @@ VERSION="v."$MAJOR"."$MINOR"."$BUMPED
 ;;
 esac
 
+# pull everything from git first
 echo "pulling everything from git first"
 git pull origin master
 
-echo "pushing all the comments for the tag ($VERSION) into releases/$VERSION.txt"
-git log $CURRENTVERSION..HEAD > /mnt/build/connecting_travel/src/releases/$VERSION.txt
+# create a release log
+echo "create log of all the comments for the tag ($VERSION) into releases/$VERSION.txt"
+git log $CURRENTVERSION..HEAD > $DIR/releases/$VERSION.txt
 
+# add all the git files
 echo "adding all files and commiting..."
 git add .
 git commit -m "$1 release ($VERSION)"
 
+# tag the branch
 echo "tagging code with version '$VERSION' and with the following message '$1 release ($VERSION)'"
 git tag -a $VERSION -m "$1 release ($VERSION)"
 
-echo "pushing everying to github origin master..."
+# push all the content back to git
+echo "pushing everying to git repo origin master..."
 git push origin master
 
+# all done!
 echo "all done.."
